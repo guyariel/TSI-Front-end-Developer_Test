f3a6568af3dbfb66150171dc898ef4845034f08d
guyariel <guy.hazoume@epitech.eu>
2023-11-28 12:56:55 +0100

    add the script for API event integration

---
+++ script.js
@@ -0,0 +1,88 @@
+const fetchButton = document.getElementById('fetchData');
+const IdInput = document.getElementById('IdInput');
+const DataDiv = document.getElementById('Data');
+
+fetchButton.addEventListener('click', () => {
+    const gameId = IdInput.value.trim(); 
+  
+    
+    if (gameId) {
+      fetchGameData(gameId);
+    } else {
+      DataDiv.textContent = 'Veuillez entrer un ID de jeu valide.';
+    }
+});
+  
+function fetchGameData(gameId) {
+    fetch(`https://www.balldontlie.io/api/v1/games/${gameId}`)
+      .then(response => {
+        if (!response.ok) {
+          throw new Error('Network response was not ok');
+        }
+        return response.json();
+      })
+        .then(data => {
+
+        
+        console.log(data); 
+            
+        const event_date = data.date;
+
+        const homeTeamName = data.home_team.full_name;
+        const homeTeamConf = data.home_team.full_name;
+        const homeTeamScore = data.home_team_score;
+        const homeTeamCity = data.home_team.city;
+
+        const visitorTeamName = data.visitor_team.full_name;
+        const visitorTeamConf = data.home_team.full_name;
+        const visitorTeamScore = data.visitor_team_score;
+
+        const GameDetailsHTML = 
+        `
+            <div class="item-container item-container-soccer">
+                <div class="img-container">
+                    <img src="./src/images/event-1.jpeg" alt="Event image">
+                </div>
+
+                <div class="body-container">
+                    <div class="overlay"></div>
+
+                    <div class="event-info">
+                        <p class="title">NBA Game</p>
+                        <div class="separator"></div>
+                        <p class="info">${homeTeamCity} , US</p>
+                        <p class="price">Passed</p>
+
+                        <div class="additional-info">
+                            <p class="info">
+                                <i class="fas fa-map-marker-alt "></i>
+                                ${homeTeamCity} Stadium 
+                            </p>
+                            <p class="info">
+                                <i class="far fa-calendar-alt "></i>
+                                ${event_date}   
+                            </p>
+
+                            <p class="decription info"> 
+                            There are the details about the ${gameId}th game's ID
+                            An intense matchup unfolded between the ${homeTeamName} from the ${homeTeamConf}, scoring ${homeTeamScore}, 
+                            and the visiting ${visitorTeamName} from the ${visitorTeamConf}, tallying ${visitorTeamScore}. <span> more...</span>
+                            </p>
+                        </div>
+                    </div>
+                    <button class="action">Book it</button>
+                </div> 
+            </div>
+        `
+        
+        const container = document.querySelector('.fetch-container');
+
+        container.innerHTML = GameDetailsHTML;
+
+      })
+      
+      .catch(error => {
+        console.error('There was a problem with the fetch operation:', error);
+        DataDiv.textContent = 'Une erreur est survenue lors de la récupération des données.';
+    });
+}
